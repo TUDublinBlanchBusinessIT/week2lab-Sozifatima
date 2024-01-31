5366c3a902efff3691d4031bf862ab6e2c9417d9
B00158031 Sozi Fatima <B00158031@mytudublin.ie>
2024-01-31 12:52:36 +0000

    test2

---
+++ partA.py
@@ -0,0 +1,16 @@
+#sozi Fatima
+#31/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printsteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printsteps()
+print("Step 7")
+print("Step 8")
+print("Step 9")
